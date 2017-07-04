3a580a5242999cc898910eebf5079d57bf65c5a9
Dawid Kurek <dawikur@gmail.com>
2017-07-04 19:42:12 +0200

    Remove BLL test

--- test/benchmark/simple_expression.cpp
+++ test/benchmark/simple_expression.cpp
@@ -7,29 +7,6 @@
 
 #include <cstdlib>
 
-#ifdef LEL_HAS_BOOST
-
-#include <boost/tuple/tuple.hpp>
-#include <boost/lambda/lambda.hpp>
-
-void simple_expression__bll(::benchmark::State &state) {
-  using namespace boost::lambda;
-
-  auto const expression = _1 * _2;
-
-  std::vector<int> vec(100);
-  std::generate(vec.begin(), vec.end(), std::rand);
-
-  while (state.KeepRunning()) {
-    for (int i = 0; i <= state.range(0); ++i) {
-      std::transform(vec.begin(), vec.end(), vec.begin(), expression);
-    }
-  }
-}
-BENCHMARK(simple_expression__bll)->Range(0, 20);
-
-#endif  // LEL_HAS_BOOST
-
 void simple_expression__lel(::benchmark::State &state) {
   using namespace LeL::Placeholders;
 
