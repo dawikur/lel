1b230c325d0afbc2c12936518a793d7d5bec3611
Dawid Kurek <dawikur@gmail.com>
2017-07-04 20:40:06 +0200

    Add BLL to test

--- test/benchmark/simple_expression_in_loop.cpp
+++ test/benchmark/simple_expression_in_loop.cpp
@@ -25,3 +25,13 @@ auto eLambda = [](auto const _x) { return _x * _x + 3; };
 
 BENCHMARK_CAPTURE(simple_expression_in_loop, LeL, eLeL)->Range(0, 100);
 BENCHMARK_CAPTURE(simple_expression_in_loop, Lambda, eLambda)->Range(0, 100);
+
+#ifdef LEL_HAS_BOOST
+
+#include <boost/lambda/lambda.hpp>
+
+auto eBLL   = boost::lambda::_1 * boost::lambda::_1 + 3;
+
+BENCHMARK_CAPTURE(simple_expression_in_loop, BLL, eBLL)->Range(0, 100);
+
+#endif  // LEL_HAS_BOOST
