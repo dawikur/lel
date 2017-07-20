42df1089d6818d76d28dd75326ecbba1a680267f
Dawid Kurek <dawikur@gmail.com>
2017-07-20 19:54:34 +0200

    Add getting type from variadic by idx

--- include/lel/variadic.hpp
+++ include/lel/variadic.hpp
@@ -10,30 +10,46 @@
 namespace LeL {
 
 struct Variadic {
+ private:
+  template <int, class...>
+  struct get_type;
+
+ public:
   template <int Num>
   struct Get {
     template <class... Values>
-    static constexpr decltype(auto) From(Values &&... values) {
+    static constexpr decltype(auto) Value(Values &&... values) {
       static_assert(Num < sizeof...(values), "Index out of range.");
 
-      return get_impl(Box<int, Num>(), std::forward<Values>(values)...);
+      return get_value(Box<int, Num>(), std::forward<Values>(values)...);
     }
+
+    template <class... Types>
+    using Type = typename get_type<Num, Types...>::Result;
   };
 
  private:
   template <int Num, class Head, class... Tail>
-  static constexpr decltype(auto) get_impl(Box<int, Num>,
-                                           Head &&,
-                                           Tail &&... tail) {
-    return get_impl(Box<int, Num - 1>(), std::forward<Tail>(tail)...);
+  static constexpr decltype(auto) get_value(Box<int, Num>,
+                                            Head &&,
+                                            Tail &&... tail) {
+    return get_value(Box<int, Num - 1>(), std::forward<Tail>(tail)...);
   }
 
   template <class Head, class... Tail>
-  static constexpr decltype(auto) get_impl(Box<int, 0>,
-                                           Head &&head,
-                                           Tail &&...) {
+  static constexpr decltype(auto) get_value(Box<int, 0>,
+                                            Head &&head,
+                                            Tail &&...) {
     return std::forward<Head>(head);
   }
+
+  template <int Num, class Head, class... Tail>
+  struct get_type<Num, Head, Tail...> : public get_type<Num - 1, Tail...> {};
+
+  template <class Head, class... Tail>
+  struct get_type<0, Head, Tail...> {
+    using Result = Head;
+  };
 };
 
 }  // namespace LeL
--- test/unit/lel/variadic.cpp
+++ test/unit/lel/variadic.cpp
@@ -4,9 +4,18 @@
 
 #include "gtest/gtest.h"
 
+#include "lel_test.hpp"
+
 TEST(variadic_test, variadic_get_returns_proper_value) {
-  ASSERT_EQ(11, LeL::Variadic::Get<0>::From(11, 22, 33, 44, 55));
-  ASSERT_EQ(22, LeL::Variadic::Get<1>::From(11, 22, 33, 44, 55));
-  ASSERT_EQ(53, LeL::Variadic::Get<2>::From(11, 22, 53, 46, 55));
-  ASSERT_EQ(46, LeL::Variadic::Get<3>::From(11, 22, 33, 46, 55));
+  ASSERT_EQ(11, LeL::Variadic::Get<0>::Value(11, 22, 33, 44, 55));
+  ASSERT_EQ(22, LeL::Variadic::Get<1>::Value(11, 22, 33, 44, 55));
+  ASSERT_EQ(53, LeL::Variadic::Get<2>::Value(11, 22, 53, 46, 55));
+  ASSERT_EQ(46, LeL::Variadic::Get<3>::Value(11, 22, 33, 46, 55));
+}
+
+TEST(variadic_test, variadic_get_returns_proper_type) {
+  ASSERT_TYPE(int(),  (LeL::Variadic::Get<0>::Type<int, char, bool, long>()));
+  ASSERT_TYPE(char(), (LeL::Variadic::Get<1>::Type<int, char, bool, long>()));
+  ASSERT_TYPE(bool(), (LeL::Variadic::Get<2>::Type<int, char, bool, long>()));
+  ASSERT_TYPE(long(), (LeL::Variadic::Get<3>::Type<int, char, bool, long>()));
 }
