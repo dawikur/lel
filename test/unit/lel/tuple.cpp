9ba0b98e728621c81b768f4fd3e222b319d20361
Dawid Kurek <dawikur@gmail.com>
2017-08-04 09:52:17 +0200

    Remove unused code

--- test/unit/lel/tuple.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2017, Dawid Kurek, <dawikur@gmail.com>
-
-#include "lel/tuple.hpp"
-
-#include "gtest/gtest.h"
-
-TEST(tuple_test, TODO) {}
-
--- test/unit/lel/variadic.cpp
+++ test/unit/lel/variadic.cpp
@@ -6,22 +6,6 @@
 
 #include "lel_test.hpp"
 
-namespace {
-
-template <class ...>
-struct List {};
-
-struct Empty{};
-struct NonEmpty{ int v; };
-
-template <class Type>
-using PredBase = std::is_same<Type, int>;
-
-template <class Type>
-using PredStruct = std::is_empty<Type>;
-
-}  // namespace
-
 TEST(variadic_test, variadic_get_can_be_used_in_constexpr) {
   static_assert(LeL::Variadic::Get<0>::Value(11, 22, 33, 44, 55) == 11, "");
   static_assert(LeL::Variadic::Get<1>::Value(11, 22, 33, 44, 55) == 22, "");
@@ -34,29 +18,4 @@ TEST(variadic_test, variadic_get_returns_proper_value) {
   ASSERT_EQ(22, LeL::Variadic::Get<1>::Value(11, 22, 33, 44, 55));
   ASSERT_EQ(53, LeL::Variadic::Get<2>::Value(11, 22, 53, 46, 55));
   ASSERT_EQ(46, LeL::Variadic::Get<3>::Value(11, 22, 33, 46, 55));
-}
-
-TEST(variadic_test, variadic_get_returns_proper_type) {
-  ASSERT_TYPE(int(),     (LeL::Variadic::Get<0>::Type<int, char, bool, int64_t>()));
-  ASSERT_TYPE(char(),    (LeL::Variadic::Get<1>::Type<int, char, bool, int64_t>()));
-  ASSERT_TYPE(bool(),    (LeL::Variadic::Get<2>::Type<int, char, bool, int64_t>()));
-  ASSERT_TYPE(int64_t(), (LeL::Variadic::Get<3>::Type<int, char, bool, int64_t>()));
-}
-
-TEST(variadic_test, variadic_filter_of_basic_types) {
-  using Input = List<int, bool, int, int64_t>;
-  using Output = List<bool, int64_t>;
-
-  using Result = LeL::Variadic::Filter<Input>::With<PredBase>;
-
-  ASSERT_TYPE(Output(), Result());
-}
-
-TEST(variadic_test, variadic_filter_of_structs) {
-  using Input = List<Empty, Empty, NonEmpty, Empty, NonEmpty>;
-  using Output = List<NonEmpty, NonEmpty>;
-
-  using Result = LeL::Variadic::Filter<Input>::With<PredStruct>;
-
-  ASSERT_TYPE(Output(), Result());
 }
