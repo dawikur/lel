7a260ac2a6aa260ed91ceae49bea01c3cdee3d4c
Dawid Kurek <dawikur@gmail.com>
2017-08-04 14:02:28 +0200

    Remove problemanic on VS test

--- test/unit/lel/operation/unary.cpp
+++ test/unit/lel/operation/unary.cpp
@@ -9,11 +9,3 @@ TEST(unary_test, logical_not_can_be_used_in_constexpr) {
 
   static_assert(logical_not(false), "");
 }
-
-TEST(unary_test, indirection_can_be_used_in_constexpr) {
-  LeL::Operation::__Indirection indirection;
-
-  int const val = 5;
-
-  static_assert(indirection(&val) == 5, "");
-}
