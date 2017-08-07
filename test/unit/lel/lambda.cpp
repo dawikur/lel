668df62439bef112a0e1ff875a4d7c4460683f69
Dawid Kurek <dawikur@gmail.com>
2017-08-07 09:04:02 +0200

    Add first tests for passthrou lambda

--- test/unit/lel/lambda.cpp
+++ test/unit/lel/lambda.cpp
@@ -4,4 +4,15 @@
 
 #include "gtest/gtest.h"
 
-TEST(lambda_test, TODO) {}
+class lambda_test : public ::testing::Test {
+ protected:
+  using Empty = LeL::Lambda<LeL::Context<LeL::Identity>, LeL::Box<char>>;
+};
+
+TEST_F(lambda_test, pasthou_lambda_has_size_1) {
+  ASSERT_EQ(1, sizeof(Empty));
+}
+
+TEST_F(lambda_test, pastthou_lambda_is_empty) {
+  ASSERT_TRUE(std::is_empty<Empty>::value);
+}
