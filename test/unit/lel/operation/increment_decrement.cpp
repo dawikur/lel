c05372cd5f82e1463c0b918ccdb208fbaa17f73c
Dawid Kurek <dawikur@gmail.com>
2017-10-20 19:27:41 +0200

    Add tests for pre/post inc/decrement

--- test/unit/lel/operation/increment_decrement.cpp
+++ test/unit/lel/operation/increment_decrement.cpp
@@ -4,4 +4,34 @@
 
 #include "gtest/gtest.h"
 
-TEST(increment_decrement_test, TODO) {}
+TEST(increment_decrement_test, preincrement) {
+  LeL::Operation::__PreIncrement pre_increment;
+
+  int i = 5;
+
+  ASSERT_EQ(6, pre_increment(i));
+}
+
+TEST(increment_decrement_test, postincrement) {
+  LeL::Operation::__PostIncrement post_increment;
+
+  int i = 5;
+
+  ASSERT_EQ(5, post_increment(i));
+}
+
+TEST(increment_decrement_test, predecrement) {
+  LeL::Operation::__PreDecrement pre_decrement;
+
+  int i = 5;
+
+  ASSERT_EQ(4, pre_decrement(i));
+}
+
+TEST(increment_decrement_test, postdecrement) {
+  LeL::Operation::__PostDecrement post_decrement;
+
+  int i = 5;
+
+  ASSERT_EQ(5, post_decrement(i));
+}
