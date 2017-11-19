b2e11606e37805edd2ee2f16eb9cd8dcb09de681
Dawid Kurek <dawikur@gmail.com>
2017-11-19 19:13:47 +0100

    Try to fix lambda tests

--- test/unit/lel/lambda.cpp
+++ test/unit/lel/lambda.cpp
@@ -18,54 +18,31 @@ using namespace ::testing;
 
 class lambda_test : public ::testing::Test {
  protected:
-  struct MockImpl {
-    MOCK_CONST_METHOD0(slice0, int());
-    MOCK_CONST_METHOD1(slice1, int(int));
-  };
-
-  template <class FuncT, FuncT Func>
-  struct MockView {
-    MockImpl& operator()() const {
-      static MockImpl impl;
-      return impl;
-    }
+  struct Mock {
+    Mock(int ret) : ret{ ret } {}
 
     template <class Slice, class ... Args>
-    auto slice(Slice, Args... args) const {
-      return (operator()().*Func)(args...);
+    auto& slice(Slice, Args... ) const {
+      return ret;
     }
+
+    int const ret;
   };
 };
 
-#define MOCK(ID, ARGS) \
-  MockView<decltype(&MockImpl::slice##ID), &MockImpl::slice##ID> mock; \
-  EXPECT_CALL(mock(), slice##ID(ARGS))
-
 TEST_F(lambda_test, when_no_ids_and_view_doesnt_have_arguments_view_is_called) {
-  MOCK(0, )
-    .WillOnce(Return(5));
-
-  LeL::Lambda<LeL::Context<LeL::Identity, decltype(mock)>,
-              LeL::Template::Box<char>>
-    lambda(mock);
+  LeL::Lambda<LeL::Context<LeL::Identity, Mock>, LeL::Template::Box<char>> lambda(Mock(5));
 
   auto result = lambda();
   ASSERT_EQ(5, result);
 }
 
 TEST_F(lambda_test, when_no_ids_view_can_be_called_with_arguments) {
-  MOCK(1, 1)
-    .WillOnce(Return(7));
-
-  LeL::Lambda<LeL::Context<LeL::Identity, decltype(mock)>,
-              LeL::Template::Box<char, '0'>>
-    lambda(mock);
+  LeL::Lambda<LeL::Context<LeL::Identity, Mock>, LeL::Template::Box<char, '0'>> lambda(Mock(7));
 
   auto result = lambda(1);
   ASSERT_EQ(7, result);
 }
-
-#undef MOCK
 
 #endif  // IS_TEST_SUPPORTED
 
