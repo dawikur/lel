74c182b04ea8ea065279ea0cfc3ff0c846518ed7
Dawid Kurek <dawikur@gmail.com>
2018-04-18 11:45:28 +0200

    Try to fix VS warning

--- include/lel/lambda.hpp
+++ include/lel/lambda.hpp
@@ -26,8 +26,10 @@ class Lambda<Context<Func, Views...>, Template::Box<Compare, IDT, IDs...>> {
   template <class... Values>
   constexpr decltype(auto) operator()(Values &&... values) const {
     return call(Template::MakeSequence<sizeof...(Views),
-                                       sizeof...(Values) - sizeof...(IDs)
-                                         + sizeof...(Views)>(),
+                                       sizeof...(Values)
+                                         + sizeof...(Views)
+                                         - sizeof...(IDs)
+                                       >(),
                 std::forward<Values>(values)...);
   }
 
